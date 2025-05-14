cf6dcbc79db3c800af3f77ed432c534365434dc0
Michael Smith <finedesignz@users.noreply.github.com>
2025-05-14 11:50:26 -0700

    format prettier

--- drizzle/relations.ts
+++ drizzle/relations.ts
@@ -1,21 +1,24 @@
-import { relations } from "drizzle-orm/relations";
-import { usersInAuth, profiles, stripeCustomers } from "./schema";
+import { relations } from "drizzle-orm/relations"
+import { usersInAuth, profiles, stripeCustomers } from "./schema"
 
-export const profilesRelations = relations(profiles, ({one}) => ({
-	usersInAuth: one(usersInAuth, {
-		fields: [profiles.id],
-		references: [usersInAuth.id]
-	}),
-}));
+export const profilesRelations = relations(profiles, ({ one }) => ({
+  usersInAuth: one(usersInAuth, {
+    fields: [profiles.id],
+    references: [usersInAuth.id],
+  }),
+}))
 
-export const usersInAuthRelations = relations(usersInAuth, ({many}) => ({
-	profiles: many(profiles),
-	stripeCustomers: many(stripeCustomers),
-}));
+export const usersInAuthRelations = relations(usersInAuth, ({ many }) => ({
+  profiles: many(profiles),
+  stripeCustomers: many(stripeCustomers),
+}))
 
-export const stripeCustomersRelations = relations(stripeCustomers, ({one}) => ({
-	usersInAuth: one(usersInAuth, {
-		fields: [stripeCustomers.userId],
-		references: [usersInAuth.id]
-	}),
-}));
+export const stripeCustomersRelations = relations(
+  stripeCustomers,
+  ({ one }) => ({
+    usersInAuth: one(usersInAuth, {
+      fields: [stripeCustomers.userId],
+      references: [usersInAuth.id],
+    }),
+  }),
+)
